df8983ce4e9b4f83ee74e811bb9f3c78f31d959d
NEZ04 <shubhams.dd24.cs@nitp.ac.in>
2026-08-24 15:35:40 +0530

    custom api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: ".env" })
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED !!", err)
+})
